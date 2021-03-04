c5265a2cbe3cc6adf3b2167843176ce112b983a3
yuanX-1005 <yefangyuanlisa@gmail.com>
2021-03-04 11:39:43 +0100

    test

--- app/src/main/java/com/example/projet/DataBaseHelper.java
+++ app/src/main/java/com/example/projet/DataBaseHelper.java
@@ -19,6 +19,8 @@ public class DataBaseHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL(SQL_CREATE_ENTRIES);
+
+        //shfkdlf
     }
 
     @Override
