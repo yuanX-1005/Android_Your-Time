e4f13ee3a26e91331e966c2a71b3be1d4f354d08
chelsm <chelseymillo@gmail.com>
2021-03-04 11:11:22 +0100

    Update MainActivity.kt

--- app/src/main/java/com/example/projet/MainActivity.kt
+++ app/src/main/java/com/example/projet/MainActivity.kt
@@ -1,10 +1,7 @@
 package com.example.projet
 
 import android.content.ContentValues
-import android.content.Intent
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -17,13 +14,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val button_commencer: Button = findViewById(R.id.button_commencer)
-        button_commencer.setOnClickListener(View.OnClickListener {
-            val intent= Intent(this, Commencer::class.java)
-            startActivity(intent)
-        })
-
 
         val dbHelper = DataBaseHelper(this)
         // Gets the data repository in write mode
